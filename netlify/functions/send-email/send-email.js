6e21aa9978c95a8a5c9f0b85c3d686ab1bef40ba
Alan Killian <akillian@outlook.com>
2022-11-30 18:31:35 -0500

    commit

--- netlify/functions/send-email/send-email.js
+++ netlify/functions/send-email/send-email.js
@@ -1,5 +1,5 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-handler = async function (event, context) {
+const handler = async function (event, context) {
     return {
       statusCode: 200,
       body: JSON.stringify({ message: "Hello World" }),
