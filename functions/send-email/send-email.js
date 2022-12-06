108a38600f4d408ec849a507439490db473dea41
Alan Killian <akillian@outlook.com>
2022-12-06 18:22:50 -0500

    commit

--- functions/send-email/send-email.js
+++ functions/send-email/send-email.js
@@ -4,7 +4,6 @@ const nodemailer = require('nodemailer');
 const handler = async (event, context, callback) => {
     // Parse the JSON text received.
     const body = JSON.parse(event.body);
-    console.log('Password', process.env.SMTP_PASSWORD);
 
     // Build an HTML string to represent the body of the email to be sent.
     const html = `<div style="margin: 20px auto;">${body.message}</div>`;
@@ -12,25 +11,25 @@ const handler = async (event, context, callback) => {
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        service: 'gmail',
         auth: {
             user: 'akillian@scarsdaleschools.org',
             pass: process.env.SMTP_PASSWORD,
         },
     });
+    console.log("Body Object is: ", body);
+    const mailOptions = {
+        from: body.email,
+        to: 'akillian@outlook.com',
+        subject: body.subject,
+        text: body.message,
+        html: html,
+    };
 
     try {
         // send mail with defined transport object
-        let info = await transporter.sendMail({
-            from: body.name,
-            to: 'akillian@outlook.com',
-            subject: body.subject,
-            text: body.message,
-            html: html,
-        });
-        // Log the result
-        console.log(info);
-        callback(null, { statusCode: 200, body: JSON.stringify(info) });
+        transporter.sendMail(mailOptions);
+
+        callback(null, { statusCode: 200, body: JSON.stringify(body) });
     } catch (error) {
         // Catch and log error.
         callback(error);
